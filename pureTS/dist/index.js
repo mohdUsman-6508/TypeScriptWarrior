b7f2fae84996d3c09cc79ada3c69c9e6823678cd
Mohammad Usman <uk351675@gmail.com>
2024-07-02 11:12:50 +0530

    add basic setup

---
+++ pureTS/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// here we are configuring the tsconfig according to our need
+// tsc -w , then index.js will be in outdir
+console.log("My name is Osman");
---
+++ pureTS/src/index.ts
@@ -0,0 +1,3 @@
+// here we are configuring the tsconfig according to our need
+// tsc -w , then index.js will be in outdir
+console.log("My name is Osman");
